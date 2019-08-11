b6612a78dc931ca676adabb543ea118856e7f417
ncelebic <nick@celebic.net>
2019-08-11 12:12:56 -0400

    Config refactor to add environment vars for docker compose

--- watcher.py
+++ watcher.py
@@ -3,6 +3,8 @@
 import time
 import logging
 import yaml
+from pathlib import Path
+from os import environ
 from deluge_client import DelugeRPCClient
 
 logging.basicConfig(level="INFO")
@@ -24,14 +26,46 @@ def convert(data):
     return data
 
 
-with open("config.yml", "r") as yamlfile:
-    cfg = yaml.load(yamlfile, Loader=yaml.BaseLoader)
+cfg = {}
+config_file = Path("config.yml")
+if config_file.is_file():
+    with open("config.yml", "r") as yamlfile:
+        yamlcfg = yaml.load(yamlfile, Loader=yaml.BaseLoader)
+    cfg['host'] = yamlcfg['host']
+    cfg['port'] = int(yamlcfg['port'])
+    cfg['username'] = yamlcfg['username']
+    cfg['password'] = yamlcfg['password']
+    cfg['tracker_whitelist'] = yamlcfg['tracker_whitelist']
+    cfg['testing_mode'] = yamlcfg['testing_mode']
+    cfg['retry_seconds'] = int(yamlcfg['retry_seconds'])
+
+if environ.get('DELUGE_HOST') is not None:
+    cfg['host'] = environ.get('DELUGE_HOST')
+if environ.get('DELUGE_PORT') is not None:
+    cfg['port'] = int(environ.get('DELUGE_PORT'))
+if environ.get('DELUGE_USERNAME') is not None:
+    cfg['username'] = environ.get('DELUGE_USERNAME')
+if environ.get('DELUGE_PASSWORD') is not None:
+    cfg['password'] = environ.get('DELUGE_PASSWORD')
+if environ.get('DELUGE_TRACKER_WHITELIST') is not None:
+    cfg['tracker_whitelist_csv'] = environ.get('DELUGE_TRACKER_WHITELIST')
+if environ.get('DELUGE_TESTING_MODE') is not None:
+    cfg['testing_mode'] = environ.get('DELUGE_TESTING_MODE')
+if environ.get('DELUGE_RETRY_SECONDS') is not None:
+    cfg['retry_seconds'] = int(environ.get('DELUGE_RETRY_SECONDS'))
+
+if 'tracker_whitelist_csv' in cfg:
+    items = cfg['tracker_whitelist_csv'].split(',')
+    cfg['tracker_whitelist'] = None
+    cfg['tracker_whitelist'] = []
+    for item in items:
+        cfg['tracker_whitelist'].append(item)
 
 client = DelugeRPCClient(
-    cfg['deluge']['host'],
-    int(cfg['deluge']['port']),
-    cfg['deluge']['username'],
-    cfg['deluge']['password']
+    cfg['host'],
+    cfg['port'],
+    cfg['username'],
+    cfg['password']
 )
 
 while True:
@@ -51,7 +85,7 @@ while True:
         converted_torrent_id = convert(torrent_id)
         converted_torrent = convert(torrent)
         for tracker in converted_torrent['trackers']:
-            for whitelisted_tracker in cfg['deluge']['tracker_whitelist']:
+            for whitelisted_tracker in cfg['tracker_whitelist']:
                 if whitelisted_tracker.lower() in tracker['url'].lower():
                     tracker_whitelist = True
                     continue
@@ -63,7 +97,7 @@ while True:
             continue
         print("Found torrent not in whitelist that is active and complete: %s\nid: %s" %
               (converted_torrent['name'], converted_torrent_id))
-        if cfg['deluge']['testing_mode'] == '1':
+        if cfg['testing_mode'] == '1':
             print("Action would be to pause torrent %s with id %s" % (converted_torrent['name'], converted_torrent_id))
         else:
             try:
@@ -72,4 +106,4 @@ while True:
             except client.Exception:
                 logging.error(client.Exception)
 
-    time.sleep(int(cfg['deluge']['retry_seconds']))
+    time.sleep(cfg['retry_seconds'])
